118dad05e5d5c423178e803ebcce7839d46a930f
jojofabe <jojofabe@vt.edu>
2014-10-08 00:20:00 -0700

    still need to add the animation on the text, change a few layout, improve the mobile first

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
-/**
-* JS implementation of the small time features in the index.html 
-*
-*/ 
+var x = setInterval(function(){
+    $('.animated .bounceUp').animate({
+        "color":'pink',
+        "visibility":"visible",
+        "opacity":1,
+        "-webkit-animation": "type 4s"
+    })},
+  1000);
