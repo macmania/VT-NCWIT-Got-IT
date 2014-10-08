0efc1b6bf3a85c7b858ef54a43a2fa9b4e1f78a9
jojofabe <jojofabe@vt.edu>
2014-10-08 14:01:34 -0700

    added changing description on girls

--- js/index.js
+++ js/index.js
@@ -1,8 +1,1 @@
-var x = setInterval(function(){
-    $('.animated .bounceUp').animate({
-        "color":'pink',
-        "visibility":"visible",
-        "opacity":1,
-        "-webkit-animation": "type 4s"
-    })},
-  1000);
+$('[data-typer-targets]').typer();
